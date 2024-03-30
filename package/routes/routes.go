d0f748f214b5521d96537ae0ed6ceb9047fa9907
Uber0802 <85829256+Uber0802@users.noreply.github.com>
2024-03-31 00:02:25 +0800

    Update routes.go

--- package/routes/routes.go
+++ package/routes/routes.go
@@ -6,3 +6,11 @@ import (
 
 )
 
+var RegisterAdRoutes = func(router *mux.Router) {
+	router.HandleFunc("/api/v1/ad", controllers.CreateAd).Methods("POST")
+	router.HandleFunc("/api/v1/ad", controllers.GetAdByAge).Methods("GET")
+	router.HandleFunc("/api/v1/ad", controllers.GetAdByGender).Methods("GET")
+	router.HandleFunc("/api/v1/ad", controllers.GetAdByCountry).Methods("GET")
+	router.HandleFunc("/api/v1/ad", controllers.GetAdByPlatform).Methods("GET")
+}
+
