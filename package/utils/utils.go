94c025ce036242e7b03f4ca8f87f435a15691d09
Uber0802 <85829256+Uber0802@users.noreply.github.com>
2024-04-07 00:29:10 +0800

    Update utils.go

--- package/utils/utils.go
+++ package/utils/utils.go
@@ -1,14 +1,15 @@
 package utils
 
-import(
-	"encoding/json"
+import (
+	jsoniter "github.com/json-iterator/go"
 	"io"
 	"net/http"
 )
+var json = jsoniter.ConfigCompatibleWithStandardLibrary
 
-func ParseBody(r *http.Request, x interface{}){
-	if body, err := io.ReadAll(r.Body); err == nil{
-		if err := json.Unmarshal([]byte(body), x); err != nil{
+func ParseBody(r *http.Request, x interface{}) {
+	if body, err := io.ReadAll(r.Body); err == nil {
+		if err := json.Unmarshal([]byte(body), x); err != nil {
 			return
 		}
 	}
