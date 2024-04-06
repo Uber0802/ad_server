b00c9ef980050f6e47ca36a40512624c012af8cc
Uber0802 <85829256+Uber0802@users.noreply.github.com>
2024-04-07 00:30:10 +0800

    Update model.go

--- package/models/model.go
+++ package/models/model.go
@@ -1,11 +1,14 @@
 package models
 
 import(
-	"encoding/json"
+	jsoniter "github.com/json-iterator/go"
 	"github.com/jinzhu/gorm"
 	"github.com/Uber0802/ad_server/package/config"
 	"time"
+	"sync"
+	"fmt"
 )
+var json = jsoniter.ConfigCompatibleWithStandardLibrary
 
 var db *gorm.DB
 type Conditions struct {
@@ -36,11 +39,18 @@ func (a *Ad) CreateAd() *Ad{
 	return a
 }
 
+var conditionsCache sync.Map
+
 func (a *Ad) MatchesConditions(age int, gender, country, platform string) bool {
     // Unmarshal Conditions JSON
+	cacheKey := fmt.Sprintf("%d-%s-%s-%s", age, gender, country, platform)
+    if cachedResult, ok := conditionsCache.Load(cacheKey); ok {
+        return cachedResult.(bool)
+    }
     var conditions Conditions
     json.Unmarshal([]byte(a.Conditions), &conditions)
 
+	result := true
     if !(age >= conditions.AgeStart && age <= conditions.AgeEnd) && (conditions.AgeStart != 0 || conditions.AgeEnd != 0) {
 		return false
     }
@@ -56,7 +66,8 @@ func (a *Ad) MatchesConditions(age int, gender, country, platform string) bool {
     if platform != "" && !contains(conditions.Platform, platform) && len(conditions.Platform) > 0 {
 		return false
     }
-    return true
+    conditionsCache.Store(cacheKey, result)
+    return result
 }
 
 func contains(slice []string, str string) bool {
